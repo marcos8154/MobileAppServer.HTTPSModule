c68fb426c503f47fbdf5ddaaf8f2691808f17c88
Marcos Vinicius Pereira de Oliveira <marcos8154@gmail.com>
2020-04-03 14:29:37 -0300

    initial commit

---
+++ MobileAppServer.Extensions.HttpServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MobileAppServer.Extensions.HttpServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            HttpModule module = new HttpModule("http://localhost:9000/");
+            module.Load(new ServerObjects.Server());
+
+            Console.ReadKey();
+        }
+    }
+}
